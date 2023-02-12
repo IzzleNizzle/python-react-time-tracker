ed55264eed62bbe328e19da0f9a2aa984e9c6234
Isaac <pehisaac@gmail.com>
2023-02-12 13:54:27 +0000

    fundamentals of form

--- client/src/components/FormDialog.js
+++ client/src/components/FormDialog.js
@@ -7,23 +7,10 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Box from '@mui/material/Box';
-import AccountCircle from '@mui/icons-material/AccountCircle';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-// import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 export default function FormDialog() {
     const [open, setOpen] = React.useState(false);
@@ -47,48 +34,49 @@ export default function FormDialog() {
                     <DialogContentText>
                         Customize your activity options
                     </DialogContentText>
-                    {/* <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    /> */}
-                    <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-                        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                        <Input
-                            id="standard-adornment-password"
-                            type={'text'}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                    // onClick={handleClickShowPassword}
-                                    // onMouseDown={handleMouseDownPassword}
-                                    >
-                                        {<VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
+                    <FormControl fullWidth sx={{
+                        my: 2,
+                        display: 'flex',
+                    }}
+                    >
+                        <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
+                            <TextField id="input-with-sx" variant="outlined" size="small" />
+                            <IconButton
+                                aria-label="toggle password visibility"
+                            // onClick={handleClickShowPassword}
+                            // onMouseDown={handleMouseDownPassword}
+                            >
+                                <DeleteIcon
+                                />
+                            </IconButton>
+                        </Box>
                     </FormControl>
-                    <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
-                        <TextField id="input-with-sx" variant="outlined" size="small" />
-                        <DeleteIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
-                        <TextField id="input-with-sx" variant="outlined" size="small" />
-                        <DeleteIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
-                        <TextField id="input-with-sx" variant="outlined" size="small" />
-                        <DeleteIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
-                        <TextField id="input-with-sx" variant="outlined" size="small" />
-                        <DeleteIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                    <FormControl fullWidth sx={{
+                        my: 2,
+                        display: 'flex',
+                    }}
+                    >
+                        <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
+                            <TextField id="input-with-sx" variant="outlined" size="small" />
+                            <IconButton
+                                aria-label="toggle password visibility"
+                            // onClick={handleClickShowPassword}
+                            // onMouseDown={handleMouseDownPassword}
+                            >
+                                <DeleteIcon
+                                />
+                            </IconButton>
+                        </Box>
+                    </FormControl>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-center', justifyContent: 'right' }}>
+                        <IconButton
+                            aria-label="toggle password visibility"
+                        // onClick={handleClickShowPassword}
+                        // onMouseDown={handleMouseDownPassword}
+                        >
+                            <AddCircleIcon fontSize='large'
+                            />
+                        </IconButton>
                     </Box>
                 </DialogContent>
                 <DialogActions>
