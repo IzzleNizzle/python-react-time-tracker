ee1b413d891e160441debe45fa913df06b3c6e83
Isaac P <pehisaac@gmail.com>
2023-02-14 19:06:33 +0000

    good debug state

--- client/src/components/FormDialog.js
+++ client/src/components/FormDialog.js
@@ -11,6 +11,17 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
 import FormControl from '@mui/material/FormControl';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Formik, Field, Form, FieldArray } from 'formik';
+import * as Yup from 'yup';
+import { Debug } from '../components/Debug';
+
+const initialValues = {
+    activities: [
+        {
+            name: '',
+        },
+    ],
+};
 
 export default function FormDialog() {
     const [open, setOpen] = React.useState(false);
@@ -28,62 +39,92 @@ export default function FormDialog() {
             <Button variant="outlined" onClick={handleClickOpen}>
                 Open form dialog
             </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Edit Activities</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Customize your activity options
-                    </DialogContentText>
-                    <FormControl fullWidth sx={{
-                        my: 2,
-                        display: 'flex',
-                    }}
-                    >
-                        <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
-                            <TextField id="input-with-sx" variant="outlined" size="small" />
-                            <IconButton
-                                aria-label="toggle password visibility"
-                            // onClick={handleClickShowPassword}
-                            // onMouseDown={handleMouseDownPassword}
-                            >
-                                <DeleteIcon
-                                />
-                            </IconButton>
-                        </Box>
-                    </FormControl>
-                    <FormControl fullWidth sx={{
-                        my: 2,
-                        display: 'flex',
-                    }}
-                    >
-                        <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
-                            <TextField id="input-with-sx" variant="outlined" size="small" />
-                            <IconButton
-                                aria-label="toggle password visibility"
-                            // onClick={handleClickShowPassword}
-                            // onMouseDown={handleMouseDownPassword}
-                            >
-                                <DeleteIcon
-                                />
-                            </IconButton>
-                        </Box>
-                    </FormControl>
-                    <Box sx={{ display: 'flex', alignItems: 'flex-center', justifyContent: 'right' }}>
-                        <IconButton
-                            aria-label="toggle password visibility"
-                        // onClick={handleClickShowPassword}
-                        // onMouseDown={handleMouseDownPassword}
-                        >
-                            <AddCircleIcon fontSize='large'
-                            />
-                        </IconButton>
-                    </Box>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Submit</Button>
-                </DialogActions>
-            </Dialog>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={Yup.object({
+                    activities: Yup.array().of(
+                        Yup.object({
+                            name: Yup.string().required('Activity Name Required'),
+                        })
+                    ),
+                })}
+                onSubmit={values => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                    }, 500);
+                }}
+            >
+                {({ values, isSubmitting, handleBlur, setFieldValue, errors, touched }) => (
+                    <Dialog open={open} onClose={handleClose}>
+                        <Form>
+                            <DialogTitle>Edit Activities</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    Customize your activity options
+                                </DialogContentText>
+                                <FieldArray name="activities">
+                                    {({ push, remove }) => (
+                                        <React.Fragment>
+                                            {values.activities &&
+                                                values.activities.length > 0 &&
+                                                values.activities.map((friend, index) => (
+                                                    <FormControl fullWidth sx={{
+                                                        my: 2,
+                                                        display: 'flex',
+                                                    }}
+                                                    >
+                                                        <Box sx={{ display: 'flex', alignItems: 'flex-center' }}>
+                                                            <Field name={`activities[${index}].name`}>
+                                                                {({ field, }) => (
+                                                                    <>
+                                                                        <TextField
+                                                                            id="input-with-sx"
+                                                                            variant="outlined"
+                                                                            size="small"
+                                                                            type="text"
+                                                                            placeholder="Activity"
+                                                                            {...field}
+                                                                            error={(touched?.activities?.[index] && errors?.activities?.[index]) && errors.activities[index].name}
+                                                                            helperText={(touched?.activities?.[index] && errors?.activities?.[index]) && errors.activities[index].name}
+                                                                        />
+                                                                    </>
+                                                                )}
+                                                            </Field>
+                                                            <IconButton
+                                                                aria-label="toggle password visibility"
+                                                                onClick={() => remove(index)}
+                                                            >
+                                                                <DeleteIcon
+                                                                />
+                                                            </IconButton>
+                                                        </Box>
+                                                    </FormControl>
+                                                ))}
+                                            <Box sx={{ display: 'flex', alignItems: 'flex-center', justifyContent: 'right' }}>
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={() => push({ name: '' })}
+                                                >
+                                                    <AddCircleIcon fontSize='large'
+                                                    />
+                                                </IconButton>
+                                            </Box>
+                                        </React.Fragment>
+                                    )}
+                                </FieldArray>
+                                <button type="submit" disabled={isSubmitting}>
+                                    Invite
+                                </button>
+                                <Debug />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button type="submit" disabled={isSubmitting}>Submit</Button>
+                            </DialogActions>
+                        </Form>
+                    </Dialog>
+                )}
+            </Formik>
         </div>
     );
 }
