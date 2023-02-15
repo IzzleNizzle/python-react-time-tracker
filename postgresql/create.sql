3e1499fd8517d315031daa1196b90d542f6c0088
Isaac P <pehisaac@gmail.com>
2023-02-15 17:33:25 -0500

    adding data strategy code

--- postgresql/create.sql
+++ postgresql/create.sql
@@ -1,4 +1,6 @@
--- DROP TABLE time_tracker.time_tracker;
+CREATE SCHEMA IF NOT EXISTS time_tracker AUTHORIZATION postgres;
+GRANT USAGE ON SCHEMA time_tracker TO postgres;
+GRANT SELECT ON ALL TABLES IN SCHEMA time_tracker TO postgres;
 
 CREATE TABLE IF NOT EXISTS time_tracker.time_tracker (
 	id 		SERIAL,
@@ -7,18 +9,28 @@ CREATE TABLE IF NOT EXISTS time_tracker.time_tracker (
     cognito_uuid character varying(36)
 );
 
-
 INSERT INTO time_tracker.time_tracker 
-(activity) 
-VALUES ('testing')
+(activity, cognito_uuid) 
+VALUES ('testing', '63b0f404-d3e9-4e65-8b25-378de26e8cdd');
 
+select * from time_tracker.time_tracker;
 
-select * from time_tracker.time_tracker
+CREATE TABLE IF NOT EXISTS time_tracker.activity_list (
+	id 		SERIAL,
+	activity varchar(32),
+    cognito_uuid character varying(36)
+);
 
-select * from time_tracker.time_tracker
-where activity = 'testing'
-order by date desc
+INSERT INTO time_tracker.activity_list 
+(activity, cognito_uuid) 
+VALUES ('Coding', '63b0f404-d3e9-4e65-8b25-378de26e8cdd');
 
+INSERT INTO time_tracker.activity_list 
+(activity, cognito_uuid) 
+VALUES ('Reading', '63b0f404-d3e9-4e65-8b25-378de26e8cdd');
 
--- ALTER TABLE time_tracker.time_tracker
--- ADD COLUMN cognito_uuid character varying(36);
+INSERT INTO time_tracker.activity_list 
+(activity, cognito_uuid) 
+VALUES ('Break', '63b0f404-d3e9-4e65-8b25-378de26e8cdd');
+
+select * from time_tracker.activity_list;
--- postgresql/useful_queries.sql
+++ postgresql/useful_queries.sql
@@ -1,10 +1,10 @@
 select * from time_tracker.time_tracker
 order by date desc
-limit 250
+limit 250;
 
 
 
-SELECT DISTINCT activity FROM time_tracker.time_tracker WHERE date("date") = '2023-01-15'
+SELECT DISTINCT activity FROM time_tracker.time_tracker WHERE date("date") = '2023-01-15';
 
 SELECT activity, count(*) as count, date_trunc('hour', "date") as hour FROM time_tracker.time_tracker WHERE date("date") = '2023-01-15' GROUP BY activity, hour ORDER BY count DESC;
 SELECT activity, count(*) as count, date_trunc('hour', "date") as hour FROM time_tracker.time_tracker WHERE date("date") in ( '2023-01-15', '2023-01-16', '2023-01-17') GROUP BY activity, hour ORDER BY hour DESC;
@@ -15,10 +15,10 @@ SELECT activity, count(*) as count, date_trunc('day', "date") as day, extract(do
 SELECT activity, count(*) as count, date_trunc('month', "date") as month FROM time_tracker.time_tracker GROUP BY activity, month ORDER BY count DESC;
 
 
-SELECT * FROM time_tracker.time_tracker WHERE date_trunc('day', "date") = '2023-01-15'
+SELECT * FROM time_tracker.time_tracker WHERE date_trunc('day', "date") = '2023-01-15';
 
 
-SELECT * FROM time_tracker.time_tracker WHERE date("date") = '2023-01-15'
+SELECT * FROM time_tracker.time_tracker WHERE date("date") = '2023-01-15';
 
 
 
