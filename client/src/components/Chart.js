55a02773a3610d5d0942aba7cde119ca2d4ca559
Iz P <pehisaac@gmail.com>
2023-05-12 08:31:48 +0000

    giving ghe charts better colors

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -21,8 +21,19 @@ ChartJS.register(
 );
 
 
+const colors = [
+    'rgb(230, 25, 75)',  // Red
+    'rgb(60, 180, 75)',  // Green
+    'rgb(255, 225, 25)',  // Yellow
+    'rgb(0, 130, 200)',  // Blue
+    'rgb(245, 130, 48)',  // Orange
+    'rgb(145, 30, 180)',  // Purple
+    'rgb(70, 240, 240)',  // Cyan
+    'rgb(240, 50, 230)',  // Magenta
+    'rgb(210, 245, 60)',  // Lime
+    'rgb(250, 190, 190)'  // Pink
+];
 
-const colors = ['rgb(252 111 142)', 'rgb(75, 192, 192)', 'rgb(53, 162, 235)', 'rgb(252 111 142)', 'rgb(75, 192, 192)','rgb(0 63 111)', 'rgb(184 5 190)']
 
 export default function Chart({ graphData }) {
     const options = {
